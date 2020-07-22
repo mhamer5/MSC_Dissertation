361a5f8e4ece3ce11e51d354380daceb2926b1c2
mhamer5 <62405213+mhamer5@users.noreply.github.com>
2020-07-22 17:53:44 +0100

    Update master_bash.bat

--- master_bash.bat
+++ master_bash.bat
@@ -9,7 +9,7 @@ mkdir /home/matthew/diss_runs/${args[0]}
 cd /home/matthew/diss_runs/${args[0]}
 
 echo "Step 1: Denoising" 
-vsearch --cluster_unoise /home/matthew/test_directory/testfiles_formaster_bash/4_mbc_derep.fasta --minsize ${args[1]} --unoise_alpha ${args[2]} --centroids 5_mbc_denoise.fasta
+vsearch --cluster_unoise /home/matthew/files_mbc_bash/4_mbc_derep.fasta --minsize ${args[1]} --unoise_alpha ${args[2]} --centroids 5_mbc_denoise.fasta
 
 echo "Step 2: Length Filtering"
 vsearch --fastx_filter 5_mbc_denoise.fasta --fastq_minlen 418 --fastq_maxlen 418 -fastaout 6_mbc_indelfil.fasta
@@ -33,19 +33,12 @@ echo "Step 6: OTU Delimitation"
 vsearch --cluster_size 9_mbc_beetle_coleop.fasta --sizein --relabel otu --id 0.97 --centroids ${args[0]}_otus.fasta
 
 echo "Step 7: Reads Mapping"
-vsearch --usearch_global home/matthew/files_for_master_bashscript/3_mbc_concat.fasta -db *_otus.fasta -id 0.97 -otutabout ${args[0]}_coleop_reads_map.tsv
-
+vsearch --usearch_global /home/matthew/files_mbc_bash/3_mbc_concat.fasta -db *_otus.fasta -id 0.97 -otutabout ${args[0]}_coleop_reads_map.tsv
+mv *_coleop_reads_map.tsv reads.tsv
+ 
+# I don't hink the below is 100% correct
 echo "Step 8: R Script analysis"
--figure the outputs, which all go into the r script
-
-where does taxonomy come into this? coleoptera asv filtering?
-
-needs all three variables in the r script
--metadata
--reads
--taxonomy
-
-what does the r script output? put into n/n_results?
+/home/matthew/files_mbc_bash/./analysisscript.R sampledata_matt.csv reads
 
 -also needs adipart script and check_expected_richness
 

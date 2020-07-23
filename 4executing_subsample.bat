cc398d765ed955b7ba99a61e8ed7258385c54f59
mhamer5 <62405213+mhamer5@users.noreply.github.com>
2020-07-23 10:14:40 +0100

    Update 4executing_subsample.bat

--- 4executing_subsample.bat
+++ 4executing_subsample.bat
@@ -1,10 +1,15 @@
 #! bin/bash
-./2bash_subsample.bash directory_name subsample_parameter
-./2bash_subsample.bash directory_name subsample_parameter
-./2bash_subsample.bash directory_name subsample_parameter
-./2bash_subsample.bash directory_name subsample_parameter
-./2bash_subsample.bash directory_name subsample_parameter
-./2bash_subsample.bash directory_name subsample_parameter
-./2bash_subsample.bash directory_name subsample_parameter
-./2bash_subsample.bash directory_name subsample_parameter
-./2bash_subsample.bash directory_name subsample_parameter
+./2bash_subsample.bash subsample_parameter run_number
+./2bash_subsample.bash subsample_parameter run_number
+./2bash_subsample.bash subsample_parameter run_number
+./2bash_subsample.bash subsample_parameter run_number
+./2bash_subsample.bash subsample_parameter run_number
+./2bash_subsample.bash subsample_parameter run_number
+./2bash_subsample.bash subsample_parameter run_number
+./2bash_subsample.bash subsample_parameter run_number
+./2bash_subsample.bash subsample_parameter run_number
+./2bash_subsample.bash subsample_parameter run_number
+./2bash_subsample.bash subsample_parameter run_number
+./2bash_subsample.bash subsample_parameter run_number
+./2bash_subsample.bash subsample_parameter run_number
+./2bash_subsample.bash subsample_parameter run_number

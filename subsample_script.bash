003f33cb56bbcc53c464918d226a43fa48aa1246
mhamer5 <62405213+mhamer5@users.noreply.github.com>
2020-08-07 12:50:09 +0100

    Update subsample_script.bash

--- subsample_script.bash
+++ subsample_script.bash
@@ -1,10 +1,40 @@
 #!/bin/bash
 
-echo $1 $2 $3 '> echo $1 $2 $3'
-#$2 = run number
+echo $1 $2'> echo $1 $2'
+
 args=("$@") 
+ 
+mkdir /home/matthew/diss_runs/${args[0]}
 
-path to subsample script/./subset_fasta.py -f ${args[0]} < 3_mbc_concat.fasta > 3_mbc_concat_${args[0]}%_r${args[1]}.fasta
+cd /home/matthew/diss_runs/${args[0]}
 
-vsearch --derep_fulllength 3_mbc_concat_${args[0]}%_r${args[1]}.fasta --output 4_mbc_derep_${args[0]}%_r${args[1]}.fasta --sizeout --relabel uniq
-rm 3_mbc_subsample_*
+echo "Step 1: subsample" 
+/av/vls/biotools/./subset_fasta.py -f ${args[1]} < /home/matthew/files_mbc_bash/3_mbc_concat.fasta > ${args[0]}_subsample.fasta
+
+echo "Step 2: Dereplication"
+vsearch --derep_fulllength ${args[0]}_subsample.fasta --output 4_mbc_derep.fasta --sizeout --sizein --relabel uniq
+rm ${args[0]}_subsample.fasta
+
+echo "Step 3: Denoising" 
+vsearch --cluster_unoise 4_mbc_derep.fasta --minsize 8 --unoise_alpha 2 --centroids 5_mbc_denoise.fasta
+rm 4_mbc_derep.fasta
+
+echo "Step 4: Length Filtering"
+vsearch --fastx_filter 5_mbc_denoise.fasta --fastq_minlen 418 --fastq_maxlen 418 -fastaout 6_mbc_indelfil.fasta
+rm 5_mbc_denoise.fasta
+
+echo "Step 5: Chimera Filtering"
+vsearch --uchime3_denovo 6_mbc_indelfil.fasta --nonchimeras 8_mbc_final.fasta
+rm 6_mbc_indelfil.fasta
+
+echo "Step 6: Coleoptera ASV filtering by Thomas's Script"
+/av/vls/biotools/./filter_fasta_by_fasta.py k /home/matthew/files_mbc_bash/coleop_asv_filtered.fasta < 8_mbc_final.fasta >9_mbc_beetle_coleop.fasta
+rm 8_mbc_final.fasta
+
+echo "Step 7: OTU Delimitation"
+vsearch --cluster_size 9_mbc_beetle_coleop.fasta --sizein --relabel otu --id 0.97 --centroids ${args[0]}_otus.fasta
+rm 9_mbc_beetle_coleop.fasta
+
+echo "Step 8: Reads Mapping"
+vsearch --usearch_global /home/matthew/files_mbc_bash/3_mbc_concat.fasta -db ${args[0]}_otus.fasta -id 0.97 -otutabout ${args[0]}_coleop_reads_map.tsv
+mv ${args[0]}_coleop_reads_map.tsv ${args[0]}_reads.tsv

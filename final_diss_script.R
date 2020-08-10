4393b26cb7742e97570ce870d76874d634e66460
mhamer5 <62405213+mhamer5@users.noreply.github.com>
2020-08-10 14:35:35 +0100

    Update final_diss_script.R

--- final_diss_script.R
+++ final_diss_script.R
@@ -1,10 +1,10 @@
 rm(list=ls())
-library("ggplot2")
 library("tidyverse")
 library("reshape")
 library("splitstackshape")
 library("stringr")
 library("lessR")
+library("rlang")
 #Data Wrangling----------------------------------
 stats_raw<-read.table("master_output.tsv")
 names<-c("runid", "testid", "stat_name", "stat", "p_value")
@@ -19,26 +19,118 @@ metadata_raw<-expandRows(metadata_raw, count = 5, count.is.col = TRUE, drop = TR
 all(metadata_raw[,1] %in% stats_raw[,1])
 rm(names)
 
-control_stats <- as.data.frame(slice(stats_raw, 1:9))
-control_stats$readpercent <- rep(100, 9)
+#control_stats <- as.data.frame(slice(stats_raw, 1:9))
+#control_stats$readpercent <- rep(100, 9)
+
+#Merge dataframe, unreplicated dataframe for both denoising and readpercent runs 
+data_merged<-Merge(stats_raw, metadata_raw, by="runid")
+#extract alpha=X runs 
+denoise_aX<-distinct(filter(data_merged, alpha == "X"))
+#remove alpha==X from datamerged
+data_merged<-filter(data_merged, !alpha == "X")
+#change data to be correct types
+data_merged$alpha<-as.numeric(data_merged$alpha) 
+data_merged$minsize<-as.factor(data_merged$minsize)
+
+#Function to filter for denoise runs and required statistic
+denoiseruns_filtering<-function(., stattochoose){
+  data_merged %>% 
+  slice(1:2674) %>% 
+  filter(stat_name == {{ stattochoose }} & !alpha == "X") %>%
+  distinct()
+}
+#Function to plot regression line (lm) of filtered denoise dataframe
+regression_plot_denoise <- function(dataframe, xaxis, yaxis, factor, yaxislab){
+  ggplot2::ggplot(dataframe, aes(x= {{ xaxis }},y= {{ yaxis }}, colour = {{ factor }})) + 
+    geom_point(size=4,)+
+    geom_smooth(method = lm, 
+                se=T,
+                linetype ="dashed",
+                colour = "darkred") + 
+    ylab({{ yaxislab }})
+}
+
+#Mantel
+#elevation stat
+filtered_Mantel_elevation<-denoiseruns_filtering(., "elevation")
+regression_plot_denoise(filtered_alpha.1, alpha, stat, minsize, "Mantel_elevation_stat")
+rm(filtered_Mantel_elevation)
+#spatial stat
+filtered_Mantel_spatial<-denoiseruns_filtering(., "spatial")
+regression_plot_denoise(filtered_spatial, alpha, stat, minsize, "Mantel_spatial_stat")
+rm(filtered_Mantel_spatial)
+
+#ADP
+#alpha.1
+filtered_ADP_alpha.1<-denoiseruns_filtering(., "alpha.1")
+regression_plot_denoise(filtered_ADP_alpha.1, alpha, stat, minsize, "ADP_alpha.1_stat")
+rm(filtered_ADP_alpha.1)
+#alpha.2
+filtered_ADP_alpha.2<-denoiseruns_filtering(., "alpha.2")
+regression_plot_denoise(filtered_ADP_alpha.2, alpha, stat, minsize, "ADP_alpha.2_stat")
+rm(filtered_ADP_alpha.2)
+#beta.1
+filtered_ADP_beta.1<-denoiseruns_filtering(., "beta.1")
+regression_plot_denoise(filtered_ADP_alpha.1, alpha, stat, minsize, "ADP_beta.1_stat")
+rm(filtered_ADP_beta.1)
+#beta.2
+filtered_ADP_beta.2<-denoiseruns_filtering(., "beta.2")
+regression_plot_denoise(filtered_ADP_beta.2, alpha, stat, minsize, "ADP_beta.2_stat")
+rm(filtered_ADP_beta.2)
+#gamma 
+filtered_ADP_gamma<-denoiseruns_filtering(., "gamma")
+regression_plot_denoise(filtered_ADP_gamma, alpha, stat, minsize, "ADP_gamma_stat")
+rm(filtered_ADP_gamma)
+
+#GLMM
+#Intercept
+filtered_GLMM_Intercept<-denoiseruns_filtering(., "(Intercept)")
+regression_plot_denoise(filtered_GLMM_Intercept, alpha, stat, minsize, "GLMM_Intercept_stat")
+rm(filtered_GLMM_Intercept)
+#scalealt
+filtered_GLMM_scalealt<-denoiseruns_filtering(., "scalealt")
+regression_plot_denoise(filtered_GLMM_scalealt, alpha, stat, minsize, "GLMM_scalealt_stat")
+rm(filtered_GLMM_scalealt)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#need a function to filter for readpercent runs, do not dereplicate rows
+
+
 #Exploratory stats---------
 #ReadPercent 
-readpercent_regression<-function(
+readpercent_regression<-function(filtered_dataframe, stat_name, ylabel){
+  dplyr::filtered_dataframe<-stats_raw %>% slice(298:657) %>% filter(stat_name == stat_name)
+  dplyr::filtered_dataframe$readpercent <- as.numeric(str_extract(mantel_elevation$runid, pattern = "_(.*)_") %>% gsub(pattern ="_","",.))
+  dplyr::filtered_dataframe<-filter(control_stats, stat_name == stat_name)  %>%  union(filtered_dataframe, control_stats)
   
-)
-#Mantel
-)
+  ggplot2::ggplot(filtered_dataframe, aes(x=readpercent, y= stat)) + geom_point()+
+    geom_smooth(method=lm, se=T, linetype="dashed", color="darkred") + 
+    labs(y="ylabel")
+}
 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  mantel_elevation<-stats_raw %>% slice(298:657) %>% filter(stat_name == c("elevation"))
+
+#Mantel
+readpercent_regression(mantel_elevation, elevation, mantel_elevation_stat)
+
+mantel_elevation<-stats_raw %>% slice(298:657) %>% filter(stat_name == c("elevation"))
 mantel_elevation$readpercent <- as.numeric(str_extract(mantel_elevation$runid, pattern = "_(.*)_") %>% gsub(pattern ="_","",.))
 mantel_elevation<-filter(control_stats, stat_name == "elevation") %>% union(mantel_elevation, control_stats)
 
@@ -166,61 +258,6 @@ rm(ADP_beta.2)
 #Could I turn the blocks into a single function that would plot the results for me when I parse it 
 #stats_raw$stat_name?
 
-#Denoising----------------------
-
-merged<-Merge(stats_raw, metadata_raw, by="runid")
-merged$alpha<-as.numeric(alpha)
-#mantel
-#elevation
-mantel_elevation<-merged %>% filter(stat_name == "elevation" & !alpha == "X") %>% slice(1:297)
-mantel_elevation<-mantel_elevation[!duplicated(mantel_elevation),]
-mantel_elevation$alpha<-as.numeric(mantel_elevation$alpha)
-mantel_elevation$minsize<-as.factor(mantel_elevation$minsize)
-
-ggplot(mantel_elevation, aes(x=alpha, y= stat, colour=minsize)) + geom_point()+
-  geom_smooth(method=lm, se=T, linetype="dashed", color="darkred") + 
-  labs(y="mantel_elevation_stat")
-
-#build a graph based on minsize only by alpha.2
-ADP_alpha.2_X<-merged %>% filter(stat_name == "alpha.2" & alpha == "X") %>% slice(1:297)
-ADP_alpha.2_X<-ADP_alpha.2_X[!duplicated(ADP_alpha.2_X),]
-ADP_alpha.2_X$minsize<-as.factor(ADP_alpha.2_X$minsize)
-
-graph_ADP_alpha.2_X<-ggplot(ADP_alpha.2_X, aes(x=minsize, y= stat, colour=runid)) + geom_point(size=4,)+
-  geom_smooth(method=lm, se=T, linetype="dashed", color="darkred") + 
-  labs(y="ADP_alpha.2_X")
-
-#ADP_alpha.1
-ADP_alpha.1<-merged %>% filter(stat_name == "alpha.1" & !alpha == "X") %>% slice(1:297)
-ADP_alpha.1<-ADP_alpha.1[!duplicated(ADP_alpha.1),]
-ADP_alpha.1$alpha<-as.numeric(ADP_alpha.1$alpha)
-ADP_alpha.1$minsize<-as.factor(ADP_alpha.1$minsize)
-
-ggplot(ADP_alpha.1, aes(x=alpha, y= stat, colour=minsize)) + geom_point(size=4,)+
-  geom_smooth(method=lm, se=T, linetype="dashed", color="darkred") + 
-  labs(y="ADP_alpha.1")
-
-#ADP_alpha.2
-ADP_alpha.2<-merged %>% filter(stat_name == "alpha.2" & !alpha == "X") %>% slice(1:297)
-ADP_alpha.2<-ADP_alpha.2[!duplicated(ADP_alpha.2),]
-ADP_alpha.2$alpha<-as.numeric(ADP_alpha.2$alpha)
-ADP_alpha.2$minsize<-as.factor(ADP_alpha.2$minsize)
-
-graph_ADP_alpha.2<-ggplot(ADP_alpha.2, aes(x=alpha, y= stat, colour=minsize)) + geom_point(size=4,)+
-  geom_smooth(method=lm, se=T, linetype="dashed", color="darkred") + 
-  labs(y="ADP_alpha.2")
-
-#How can I combine ADP_alpha.2 and ADP_alpha.2_X?
-
-#ADP_gamma
-ADP_gamma<-merged %>% filter(stat_name == "alpha.2" & !alpha == "X") %>% slice(1:297)
-ADP_gamma<-ADP_gamma[!duplicated(ADP_gamma),]
-ADP_gamma$alpha<-as.numeric(ADP_gamma$alpha)
-ADP_gamma$minsize<-as.factor(ADP_gamma$minsize)
-
-ggplot(ADP_gamma, aes(x=alpha, y= stat, colour=minsize)) + geom_point(size=4,)+
-  geom_smooth(method=lm, se=T, linetype="dashed", color="darkred") + 
-  labs(y="ADP_gamma")
 
 
 
